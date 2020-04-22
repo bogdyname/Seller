17a311083ae8fb4d6c5f1032d95dc81206895b9c
bogdyname <bogdyname@gmail.com>
2020-04-22 14:27:21 -0500

    added db tts code

---
+++ TTS/tts.js
@@ -0,0 +1,18 @@
+vat http = require('http');
+
+var options = {
+  host: 'localhost',
+  port: 7676,
+  path: '/?file=error',
+  method: 'GET'
+};
+
+function ProcessPublicTimeline(response)
+{
+  console.log('request done');
+}
+
+for(var i = 0; i < 2000; i++)
+{
+  http.request(options, ProcessPublicTimeline).end();
+}
