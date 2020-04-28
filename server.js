ba31acd0176b58a1c39ff728fe7ded39ca57bb13
bogdyname <bogdyname@gmail.com>
2020-04-28 22:21:21 +0300

    still learning node and js

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+//global modules
 var http = require('http');
 
+//npm modules
+var colors = require('colors');
+
+colors.setTheme({
+  warning: 'yellow',
+  error: 'red',
+  note: 'blue'
+});
+
+//own modules 
+var database = require('./database/maindb');
+
 http.createServer(function(req, res){
-  res.writeHead(200, {'content-type': 'text/plain'});
-  res.end("Server is running\n");
+  
 }).listen(7676);
 
-console.log('Server is running on port 7676');
+console.log('Server is running on port 7676'.warning);
+console.log(process.platform.note);
